997f2e97adf07a0911d35edbc7c99b32cf337ed8
killivalavan <killivalavan.exe@gmail.com>
2025-08-02 14:33:08 +0530

    Remove next-sitemap and add static sitemap

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.hiyafashions.com',
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  outDir: 'public',
-};
